8630a264a265036b4af28d997bfbfddf62a3d5ef
Sourav Sachdeva <souravsachdeva13@gmail.com>
2019-11-11 22:02:03 -0800

    creating custom post types

--- FriendlyChatSwift/HomeViewController.swift
+++ FriendlyChatSwift/HomeViewController.swift
@@ -1,17 +1,23 @@
 
 
 import UIKit
+import Kingfisher
+
 class HomeViewController:UIViewController{
 
     @IBOutlet weak var tableView: UITableView!
     var posts = [Post]()
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        configureTableView()
         UserService.posts(for: User.current) { (posts) in
             self.posts = posts
             self.tableView.reloadData()
         }
+    }
+    func configureTableView(){
+        tableView.tableFooterView = UIView()
+        tableView.separatorStyle = .none
     }
 }
 extension HomeViewController: UITableViewDataSource {
@@ -20,12 +26,18 @@ extension HomeViewController: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath)
-        cell.backgroundColor = .red
+        let post = posts[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostImageCell", for: indexPath) as? PostImageCell
+        let imageURL = URL(string:post.imageURL)
+        cell?.postimageView.kf.setImage(with:imageURL)
 
-        return cell
+        return cell!
     }
 }
 extension HomeViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+          let post = posts[indexPath.row]
 
+          return post.imageHeight
+      }
 }
---
+++ FriendlyChatSwift/PostImageCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostImageCell.swift
+//  FriendlyChatSwift
+//
+//  Created by sourav sachdeva on 11/11/19.
+//  Copyright © 2019 Google Inc. All rights reserved.
+//
+
+import UIKit
+
+class PostImageCell: UITableViewCell {
+
+    @IBOutlet weak var postimageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

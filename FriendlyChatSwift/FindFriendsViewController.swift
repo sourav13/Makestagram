9e4d4abe6b51fb58931b154b86bdf18e01441882
Sourav Sachdeva <souravsachdeva13@gmail.com>
2019-11-11 19:00:11 -0800

    Added Makestagram Structure

---
+++ FriendlyChatSwift/FindFriendsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  FindFriendsViewController.swift
+//  FriendlyChatSwift
+//
+//  Created by sourav sachdeva on 11/11/19.
+//  Copyright © 2019 Google Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class FindFriendsViewController:UIViewController{
+    
+}
---
+++ FriendlyChatSwift/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  FriendlyChatSwift
+//
+//  Created by sourav sachdeva on 11/11/19.
+//  Copyright © 2019 Google Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class HomeViewController:UIViewController{
+    
+}
